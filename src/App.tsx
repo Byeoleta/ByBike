b340b5fbec8b91af6057d9fae49599d86cdc2765
byeol <jeongeunbyeol8@gmail.com>
2022-12-28 18:31:02 +0900

    feat: BrowserView, MobileView 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>test</h1>
+        <BrowserView>데스크톱브라우져!</BrowserView>
+        <MobileView>모바일 브라우져!</MobileView>
       </header>
     </div>
   );
